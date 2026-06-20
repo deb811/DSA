06dca59d699f360b841bbbe6ead1f0b99901f992
devesh <0204anonymous@gmail.com>
2026-06-20 17:36:53 +0530

    added adjacency

---
+++ Graph/adjacentlist.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+#include<unordered_map>
+#include<list>
+
+
+class graph{
+
+    public:
+    unordered_map<int,list<int>>adj;
+
+    void addedge(int u,int v,bool direction){
+         // direction =0->undirection
+         //direction= 1->direction
+
+         adj[u].push_back(v);
+
+         if(direction==0){
+            adj[v].push_back(u);
+         }
+        }
+
+
+        void print(){
+            for(auto i : adj){
+                cout<<i.first<<"->";
+
+                for(auto j: i.second){
+                    cout<<j<<",";
+                }
+            }
+        }
+
+    
+
+};
+
+
+int main(){
+
+    int n;
+    cout<<"enter the no of nodes";
+    cin>>n;
+
+    int m;
+    cout<<"enter the no of edges";
+    cin>>m;
+
+    graph g;
+
+    for(int i=0; i<m; i++){
+        int u,v;
+        cin>>u>>v;
+
+        //creating a indirection garph
+        g.addedge(u,v,0);
+    }
+   // print the graph
+
+   g.print();
+
+   return 0;
+}
